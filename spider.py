9d276c2449977346f8991542b1233fbffa94092c
1m188 <228117330@qq.com>
2022-11-21 19:36:54 +0800

    init

---
+++ spider.py
@@ -0,0 +1,66 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'http://www.waptxt.org/96031'
+WEBSITE = 'http://www.waptxt.org'
+ENCODING = 'gbk'
+PARSER = 'lxml'
+
+
+def get_bsobj(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    response.encoding = ENCODING
+    return BeautifulSoup(response.text, PARSER)
+
+
+def get_chapter(url: str) -> str:
+    '''
+    获取章节名称及其内容
+
+    url: 章节地址
+    return: 章节名称\\n章节内容\\n\\n
+
+    其中，章节内容是每段进行分行
+    '''
+
+    soup = get_bsobj(url)
+    cont = soup.find('div', {'class': 'con_txt'})
+    title = soup.find('div', {'class': 'title'})
+
+    title_text = title.h1.text.strip()
+    cont_text = '\n'.join(cont.stripped_strings)
+
+    return title_text + '\n' + cont_text + '\n\n'
+
+
+def get_chapter_url_list() -> list[str]:
+    '''
+    获取章节url的列表
+    '''
+
+    url = URL
+    res: list[str] = []
+
+    while True:
+        soup = get_bsobj(url)
+        for i in soup.dl.findAll('a'):
+            res.append(WEBSITE + i.attrs['href'])
+
+        next_page = soup.find('span', {'class': 'right'})
+        if 'disabled' in next_page.a.attrs: break
+        url = WEBSITE + next_page.a.attrs['href']
+
+    return res
+
+
+def save_novel():
+    chapter_url_list = get_chapter_url_list()
+    with open(sys.path[0] + '/道诡异仙.txt', 'w', encoding='utf-8') as f:
+        for i in range(len(chapter_url_list)):
+            f.write(get_chapter(chapter_url_list[i]))
+            print(f'第{i}章缓存完毕')
+
+
+if __name__ == '__main__':
+    save_novel()
