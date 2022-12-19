d4d0270771a7af2be4a5907249070459fe367abc
yrret <228117330@qq.com>
2022-12-19 12:12:00 +0800

    面向对象改写

--- spider.py
+++ spider.py
@@ -8,72 +8,84 @@ ENCODING = 'gbk'
 PARSER = 'lxml'
 
 
-def get_bsobj(url: str) -> BeautifulSoup:
-    response = requests.get(url)
-    response.encoding = ENCODING
-    return BeautifulSoup(response.text, PARSER)
+class Spider:
 
+    def __init__(self, website: str, url: str, encoding: str,
+                 parser: str) -> None:
+        '''
+        @param website 小说网站地址
+        @param url 小说页面（目录页面）地址
+        @param encoding: 网站编码
+        @param parser: 解码器
+        '''
 
-def get_chapter(url: str) -> str:
-    '''
-    获取章节名称及其内容
+        self.website = website
+        self.url = url
+        self.encoding = encoding
+        self.parser = parser
 
-    url: 章节地址
-    return: 章节名称\\n章节内容\\n\\n
+    def get_bsobj(self, url: str) -> BeautifulSoup:
+        response = requests.get(url)
+        response.encoding = self.encoding
+        return BeautifulSoup(response.text, self.parser)
 
-    其中，章节内容是每段进行分行
-    '''
+    def get_chapter(self, curl: str) -> str:
+        '''
+        获取章节名称及其内容
 
-    soup = get_bsobj(url)
-    cont = soup.find('div', {'class': 'con_txt'})
-    title = soup.find('div', {'class': 'title'})
+        curl: 章节地址
+        return: 章节名称\\n章节内容\\n\\n
 
-    title_text = title.h1.text.strip()
-    cont_text = '\n'.join(cont.stripped_strings)
+        其中，章节内容是每段进行分行
+        '''
 
-    return title_text + '\n' + cont_text + '\n\n'
+        soup = self.get_bsobj(curl)
+        cont = soup.find('div', {'class': 'con_txt'})
+        title = soup.find('div', {'class': 'title'})
 
+        title_text = title.h1.text.strip()
+        cont_text = '\n'.join(cont.stripped_strings)
 
-def get_chapter_url_list(url: str, website: str) -> list[str]:
-    '''
-    获取章节url的列表
+        return title_text + '\n' + cont_text + '\n\n'
 
-    @param url 小说页面（目录页面）地址
-    @param website 小说网站地址
-    '''
+    def get_chapter_url_list(self) -> list[str]:
+        '''
+        获取章节url的列表
+        '''
 
-    res: list[str] = []
+        url = self.url
+        res: list[str] = []
 
-    while True:
-        soup = get_bsobj(url)
-        for i in soup.dl.findAll('a'):
-            res.append(website + i.attrs['href'])
+        while True:
+            soup = self.get_bsobj(url)
+            for i in soup.dl.findAll('a'):
+                res.append(self.website + i.attrs['href'])
 
-        next_page = soup.find('span', {'class': 'right'})
-        next_page_href = next_page.a.attrs.get('href', None)
-        if not next_page_href: break
-        url = website + next_page_href
+            next_page = soup.find('span', {'class': 'right'})
+            next_page_href = next_page.a.attrs.get('href', None)
+            if not next_page_href: break
+            url = self.website + next_page_href
 
-    return res
+        return res
 
+    def save_novel(self, start_chapter: int = 1):
+        '''
+        保存小说
 
-def save_novel(start_chapter: int = 1):
-    '''
-    保存小说
+        @param start_chapter 从第几章开始
+        '''
 
-    @param start_chapter 从第几章开始
-    '''
-
-    print('开始获取章节目录...')
-    chapter_url_list = get_chapter_url_list(URL, WEBSITE)
-    print('章节目录获取完毕')
-    print('开始缓存')
-    with open(sys.path[0] + '/道诡异仙.txt', 'a', encoding='utf-8') as f:
-        for i, v in enumerate(chapter_url_list[start_chapter - 1:]):
-            f.write(get_chapter(v))
-            print(f'第{i + start_chapter}章缓存完毕')
-    print('所有章节缓存完毕！')
+        print('开始获取章节目录...')
+        chapter_url_list = self.get_chapter_url_list()
+        print('章节目录获取完毕')
+        print('开始缓存')
+        with open(sys.path[0] + '/道诡异仙.txt', 'a', encoding='utf-8') as f:
+            for i, v in enumerate(chapter_url_list[start_chapter - 1:]):
+                f.write(self.get_chapter(v))
+                print(f'第{i + start_chapter}章缓存完毕')
+        print('所有章节缓存完毕！')
 
 
 if __name__ == '__main__':
-    save_novel()
+    spider = Spider(WEBSITE, URL, ENCODING, PARSER)
+    spider.save_novel()
